fed523207b62667dcbf00bddeea0af85a9655228
Sulpures <xuescchn@foxmail.com>
2018-12-01 21:29:15 -0500

    1.0
    build frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CustomerComponent } from './components/customer/customer.component';
+
+import { TransactionComponent } from './components/transaction/transaction.component';
+import { ProductComponent } from './components/product/product.component';
+import { SalespersonComponent } from './components/salesperson/salesperson.component';
+import { StoreComponent } from './components/store/store.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { AddStoreComponent } from './components/add-store/add-store.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { AddSalespersonComponent } from './components/add-salesperson/add-salesperson.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CustomerComponent,
+    TransactionComponent,
+    ProductComponent,
+    SalespersonComponent,
+    StoreComponent,
+    LoginComponent,
+    AddCustomerComponent,
+    AddStoreComponent,
+    AddProductComponent,
+    AddSalespersonComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-salesperson/add-salesperson.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-salesperson',
+  templateUrl: './add-salesperson.component.html',
+  styleUrls: ['./add-salesperson.component.css']
+})
+export class AddSalespersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
